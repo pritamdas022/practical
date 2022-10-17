92993ca71f0128e227572e807bc2856139441e2e
pritamdas022 <110466018+pritamdas022@users.noreply.github.com>
2022-10-17 22:40:46 +0530

    Update TryCatch.java

--- ExceptionHandling/TryCatch.java
+++ ExceptionHandling/TryCatch.java
@@ -6,6 +6,7 @@ public class TryCatch {
 //		try block
 		try {
 			int num = 25/0; // may exception occur
+			// if exception occurs, the remaining statement will not exceute  
 			System.out.println("tahole r print hobe na");
 //			catch block
 		} catch (Exception e) { // handling exception
